782aef619f7db47c8e36ff6b1271aa19d203d133
Jens Arps <mail@jensarps.de>
2013-03-11 21:49:27 +0100

    Inline docs for api descriptor

--- engines/api.js
+++ engines/api.js
@@ -9,18 +9,56 @@ define([], function () {
     storeId: '',
 
     isAvailable: function () {
+      //  summary:
+      //    Checks if the given engine is supported and returns false if not
+      //    and true if it is supported _and_ ready to be used.
+      //	returns: Boolean or Promise
     },
 
     put: function (id, object) {
+      //  summary:
+      //    Stores the given object. Must throw or reject if operation fails.
+      //  id: *
+      //    The id of the object to store.
+      //  object: Object
+      //    The object to store.
+      //  returns:
+      //    undefined or Promise
     },
 
     remove: function (id) {
+      //  summary:
+      //    Removes the object identified by the given id. Must throw or reject
+      //    if operation fails.
+      //  id: *
+      //    The id of the object to remove.
+      //  returns:
+      //    undefined or Promise
     },
 
     clear: function () {
+      //  summary:
+      //    Clears all data associated to the given storeId. Must throw or
+      //    reject if operation fails.
+      //  returns:
+      //    undefined or Promise
     },
 
     getAll: function () {
+      //  summary:
+      //    Retrieves all data associated to the given storeId.
+      //  returns:
+      //    Array or Promise
+    },
+
+    apply: function (data) {
+      //  summary:
+      //    Clears the storage and puts the prpvided data in it. Must throw or
+      //    reject if operation fails.
+      //  data: Array
+      //    An array of data objects.
+      //  returns:
+      //    undefined or Promise
     }
 
   };

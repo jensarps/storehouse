7327a65a3ebd976526ef886e1fc18e045e2303eb
Jens Arps <mail@jensarps.de>
2013-03-14 13:00:52 +0100

    Can't reject a promise twice

--- Storehouse.js
+++ Storehouse.js
@@ -221,10 +221,6 @@ define([
           deferred = new Deferred(),
           inst = this;
 
-      if (typeof id == 'undefined') {
-        deferred.reject(new Error('Cannot remove item: No id was provided.'));
-      }
-
       if (id in index) {
         when(this.engine.remove(id), function(){
           data.splice(index[id], 1);
@@ -234,7 +230,7 @@ define([
           deferred.reject(err)
         });
       } else {
-        deferred.reject(new Error('Cannot remove item: No object found with the given id.'));
+        deferred.reject(new Error('Cannot remove item: No id was provided or no object found with the given id.'));
       }
 
       return deferred.promise;
